65a28899447a9569b9f0264a85bee822631d2424
DorinGI <158717197+DorinGI@users.noreply.github.com>
2024-10-12 23:35:25 +0200

    Phonebook with Redux Toolkit

---
+++ src/redux/store.js
@@ -0,0 +1,39 @@
+import { configureStore } from '@reduxjs/toolkit';
+import contactsReducer from './contactsSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+
+// Configurare persist pentru redux-persist
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configurare store cu reducer persistent
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
